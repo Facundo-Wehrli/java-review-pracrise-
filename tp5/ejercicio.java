60ea1d8c673b2947c6c14dda05bdd14d524ba5ed
Facundo <wehrlifacundo9@gmail.com>
2023-01-19 21:48:44 -0300

    ejercicio1 tp5 terminado

---
+++ tp5/ejercicio.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// 1. Realizar un programa en Java que lea dos números pasados por teclado en un proceso
+// repetitivo. Este proceso terminará cuando los números leídos sean iguales.
+public class ejercicio {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        // utilizaremos la estructura repetititiva do while
+        int num1, num2;
+        do {
+            System.out.println("primer número");
+            num1 = obj.nextInt();
+            System.out.println("segundo número");
+            num2 = obj.nextInt();
+        } while (num1 != num2);
+        System.out.println("son iguales ");
+        obj.close();
+    }
+}
